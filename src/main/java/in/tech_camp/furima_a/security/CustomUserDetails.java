2c8aa671edfb5dba2a8210cbd202b9466c25fb86
kaipupupuland678 <k.hiranuma@infomart.co.jp>
2026-08-10 16:10:20 +0900

    Security

---
+++ src/main/java/in/tech_camp/furima_a/security/CustomUserDetails.java
@@ -0,0 +1,49 @@
+package in.tech_camp.furima_a.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import in.tech_camp.furima_a.entity.UserEntity;
+
+public class CustomUserDetails implements UserDetails {
+
+    // データベースから取得したユーザー情報を格納する変数
+    private final UserEntity user;
+
+    public CustomUserDetails(UserEntity user) {
+        this.user = user;
+    }
+
+    // 後でログイン中のユーザー情報（ニックネームなど）を取り出すためのメソッド
+    public UserEntity getUser() {
+        return user;
+    }
+
+    @Override
+    public String getUsername() {
+        // メールアドレスをログインIDとして使うため、emailを返す
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        // 暗号化されたパスワードを返します（Spring Securityが自動で照合してくれます）
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
+}
