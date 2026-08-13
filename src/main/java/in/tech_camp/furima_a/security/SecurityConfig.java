1cd6b2c8a84283ae01d5051c6815b10459bcbcd8
kaipupupuland678 <k.hiranuma@infomart.co.jp>
2026-08-13 13:41:41 +0900

    Basic認証の設定２

--- src/main/java/in/tech_camp/furima_a/security/SecurityConfig.java
+++ src/main/java/in/tech_camp/furima_a/security/SecurityConfig.java
@@ -6,12 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,9 +23,9 @@ public class SecurityConfig {
                 .requestMatchers("/", "/items/*", "/users/sign_in", "/users/sign_up").permitAll()
                 .requestMatchers("/items/new", "/items/*/orders").authenticated()
                 .requestMatchers(HttpMethod.POST, "/post").authenticated()
-                .anyRequest().permitAll()
+               // .anyRequest().permitAll()
             // 本番環境:全てのURLへのアクセスに認証を必須にする
-            //　.anyRequest().authenticated()
+            .anyRequest().authenticated()
             )
             // Basic認証を有効にする
             .httpBasic(Customizer.withDefaults())
@@ -56,17 +52,17 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailService(PasswordEncoder encoder) {
-      String username = System.getenv("BASIC_AUTH_USER");
-      String password = System.getenv("BASIC_AUTH_PASSWORD");
+    // @Bean
+    // public UserDetailsService userDetailService(PasswordEncoder encoder) {
+      // String username = System.getenv("BASIC_AUTH_USER");
+      // String password = System.getenv("BASIC_AUTH_PASSWORD");
 
-      UserDetails user = User.withUsername(username)
-          .password(encoder.encode(password))
-          .roles("ADMIN")
-          .build();
-      return new InMemoryUserDetailsManager(user);
-    }
+      // UserDetails user = User.withUsername(username)
+          // .password(encoder.encode(password))
+          // .roles("ADMIN")
+          // .build();
+         // return new InMemoryUserDetailsManager(user);
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
